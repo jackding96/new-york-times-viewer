41b4cf6fe7880e6312b31bdf7f60883ab94b8d62
Jack Ding <jack@Jacks-MacBook-Air.local>
2019-01-30 13:33:28 -0500

    set up listeners for when user goes to NYT article

--- background.js
+++ background.js
@@ -1,9 +1,23 @@
+// For when the user changes activeTab
 chrome.tabs.onActivated.addListener(function(activeInfo) {
 
   // Get current active tab
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
     var url = tabs[0].url;
-    console.log(url);
+    if(url.includes('nytimes.com') && url.includes('.html')) {
+      console.log(url);
+    }
   });
+});
 
+// For when the user changes URL on the activeTab
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+
+  // Only listen to the complete load event
+  if (changeInfo.status == 'complete') {
+    url = tab.url;
+    if(url.includes('nytimes.com') && url.includes('.html')) {
+      console.log(url);
+    }
+  }
 });
